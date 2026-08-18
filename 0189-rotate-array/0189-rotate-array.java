a033bb1a9a054a5b8200ffd2205cb5231c1da692
charu-codes <charumuthreja67876@gmail.com>
2026-08-18 12:14:00 +0530

    Time: 5 ms (3.38%), Space: 273 MB (8%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.java
+++ 0189-rotate-array/0189-rotate-array.java
@@ -1,17 +1,13 @@
 class Solution {
     public void rotate(int[] nums, int k) {
         int n = nums.length;
-        int s = 0;
         int[] num = new int[n];
         k = k%n;
-        for(int i=0; i<n; i++){
-            if(i<k){
-                num[i] = nums[n-k+i];
-            }
-            else{
-                num[i] = nums[s];
-                s++;
-            }
+        for(int i=0; i<k; i++){
+            num[i] = nums[n-k+i];   
+        }
+        for (int i = k; i < n; i++) {
+            num[i] = nums[i - k];
         }
         for(int i=0; i<n; i++){
             nums[i] = num[i];
